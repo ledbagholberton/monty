66cfc40ebd80cb1f43ee1c7fa0d3a8a1806d33a7
Giovanny Perez <789@holbertonschool.com>
2019-05-15 19:14:29 +0000

    [Fix] Issue related with EOF

--- monty.c
+++ monty.c
@@ -1,5 +1,4 @@
 #include "monty.h"
-
 /**
  * main - entry point
  * @argc: argument count
@@ -7,14 +6,11 @@
  *
  * Return: 0 or exit error
  */
-
-int num;
+int num = 0;
 int main(int argc, char **argv)
 {
-	
 	int fd, num_letters, j, i;
-	char tmp[1000];
-	char *buf = malloc(sizeof(char) * 1024);
+	char tmp[1000], *buf = malloc(sizeof(char) * 1024);
 	stack_t *head;
 	unsigned int line = 0;
 
@@ -25,14 +21,12 @@ int main(int argc, char **argv)
 		free(buf);
 		exit(EXIT_FAILURE);
 	}
-
 	fd = open(argv[1], O_RDONLY);
-
 	if (!argv[1] || fd == -1)
 	{
 		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
 		free(buf);
-		exit (EXIT_FAILURE);
+		exit(EXIT_FAILURE);
 	}
 	num_letters = read(fd, buf, sizeof(char) * 1024);
 	if (num_letters == -1)
@@ -41,21 +35,20 @@ int main(int argc, char **argv)
 		return (0);
 	}
 	j = 0;
-	while(buf[j] != '\0')
+	while (buf[j] != '\0')
 	{
 		i = 0;
-		for(; buf[j] != '\n'; j++, i++)
+		for (; buf[j] != '\n' && buf[j] != '\0'; j++, i++)
 			tmp[i] = buf[j];
-		if (buf[j + 1] != '\0' || buf[j] != '\n' || buf[j] != 0)
+		tmp[i] = '\0';
+		printf("%s\n", tmp);
+		if (tmp[0] != '\0')
 		{
-			tmp[i] = '\0';
-			printf("%s\n", tmp);
 			line++;
 			exec_comp(tmp, &head, line);
 		}
 		j++;
 	}
-
 	close(fd);
 	free_dlistint(head);
 	free(buf);
